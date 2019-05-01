0f4b9ef1c99405e11290b113092b41e2dad2f589
Jerred Shepherd <ShepherdJerred@gmail.com>
2019-05-01 17:08:47 -0500

    Add tax calculation

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import simulator
 
+tax_rate = .3
 simulation_length_in_months = 48
 
-simulator.run_simulation(simulation_length_in_months)
+simulator.run_simulation(simulation_length_in_months, tax_rate)
